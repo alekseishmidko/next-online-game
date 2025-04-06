0ca2d703f3758fbc0a66310ce9c31af847e1e46e
alekseishmidko <alexeishmidko@gmail.com>
2025-04-06 20:54:43 +0400

    features/games-list

---
+++ src/shared/lib/sse/client.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+export function useEventsSource<T>(url: string, onData?: (data: T) => void) {
+  const [isPending, setIsPending] = useState(true);
+  const [data, setData] = useState<T>();
+  const [error, setError] = useState<unknown | undefined>();
+
+  useEffect(() => {
+    const gameEvents = new EventSource(url);
+
+    gameEvents.addEventListener("message", (message) => {
+      try {
+        const data = JSON.parse(message.data);
+        setError(undefined);
+        setData(data);
+        onData?.(data);
+        setIsPending(false);
+      } catch (e) {
+        setError(e);
+      }
+    });
+
+    gameEvents.addEventListener("error", (e) => {
+      setError(e);
+    });
+
+    return () => gameEvents.close();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [url]);
+
+  return {
+    dataStream: data,
+    error,
+    isPending,
+  };
+}
